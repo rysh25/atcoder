d17a3ba7d428dc70abbb3a4a4aa42758b55125d1
Ryohei Shiraki <rysh25@gmail.com>
2023-03-26 10:02:58 +0900

    Add joi2007ho-C

--- joi2007ho/c.cpp
+++ joi2007ho/c.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -8,14 +9,36 @@ int main()
     int N;
     cin >> N;
 
-    vector<pair<int, int>> pillars(N);
+    vector<pair<int, int>> p;
+    set<pair<int, int>> s;
 
-    int res = 0;
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < N; i++)
     {
         int x, y;
         cin >> x >> y;
-        pillars[i] = make_pair(x, y);
+        p.emplace_back(x, y);
+        s.emplace(x, y);
+    }
+
+    int res = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            int dx = p[i].first - p[j].first;
+            int dy = p[i].second - p[j].second;
+
+            if (s.count(make_pair(p[i].first - dy, p[i].second + dx)) &&
+                s.count(make_pair(p[j].first - dy, p[j].second + dx)))
+            {
+                res = max(res, abs(dx * dx) + abs(dy * dy));
+            }
+            if (s.count(make_pair(p[i].first + dy, p[i].second - dx)) &&
+                s.count(make_pair(p[j].first + dy, p[j].second - dx)))
+            {
+                res = max(res, abs(dx * dx) + abs(dy * dy));
+            }
+        }
     }
 
     cout << res << endl;
