68c35fc12216a40d00ca55d294c5fb72937716de
Ryohei Shiraki <rysh25@gmail.com>
2023-05-03 17:00:07 +0900

    Fix ABC299-E

--- abc/abc299/e.cpp
+++ abc/abc299/e.cpp
@@ -5,74 +5,82 @@
 
 using namespace std;
 
-vector<int> bfs(const vector<vector<int>> &graph, int start)
-{
-    vector<int> dist(graph.size(), -1);
-    dist[start] = 0;
-    queue<int> q;
-    q.push(start);
-
-    while (!q.empty())
-    {
-        int u = q.front();
-        q.pop();
-        for (int v : graph[u])
-        {
-            if (dist[v] == -1)
-            {
-                dist[v] = dist[u] + 1;
-                q.push(v);
-            }
-        }
-    }
-    return dist;
-}
-
 int main()
 {
     int N, M;
     cin >> N >> M;
 
-    vector<vector<int>> graph(N);
+    vector<vector<int>> G(N);
     for (int i = 0; i < M; i++)
     {
         int u, v;
         cin >> u >> v;
         u--;
         v--;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
+        G[u].emplace_back(v);
+        G[v].emplace_back(u);
     }
 
     int K;
     cin >> K;
-    vector<pair<int, int>> pd_list(K);
+    vector<int> p(K), d(K);
     for (int i = 0; i < K; i++)
     {
-        cin >> pd_list[i].first >> pd_list[i].second;
-        pd_list[i].first--;
+        cin >> p[i] >> d[i];
+        p[i]--;
     }
 
-    string ans(N, '0');
-    for (const auto &pd : pd_list)
+    int INF = 1e9;
+    auto bfs = [&](int sv)
     {
-        ans[pd.first] = '1';
-    }
-
-    for (const auto &pd : pd_list)
-    {
-        int p = pd.first, d = pd.second;
-        vector<int> dist = bfs(graph, p);
-        bool flag = false;
-        for (int u = 0; u < N; u++)
+        vector<int> dist(N, INF);
+        dist[sv] = 0;
+        queue<int> q;
+        q.push(sv);
+        while (q.size())
         {
-            if (ans[u] == '1' && dist[u] == d)
+            int v = q.front();
+            q.pop();
+            for (int u : G[v])
             {
-                flag = true;
-                break;
+                if (dist[u] == INF)
+                {
+                    dist[u] = dist[v] + 1;
+                    q.push(u);
+                }
             }
         }
-        if (!flag)
+        return dist;
+    };
+
+    vector<vector<int>> gs(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        gs[i] = bfs(i);
+    }
+
+    string ans(N, '1');
+
+    for (int i = 0; i < K; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (gs[p[i]][j] < d[i])
+                ans[j] = '0';
+        }
+    }
+
+    for (int i = 0; i < K; i++)
+    {
+        int now = INF;
+        for (int j = 0; j < N; j++)
+        {
+            if (ans[j] == '1')
+                now = min(now, gs[p[i]][j]);
+        }
+
+        if (now != d[i])
         {
             cout << "No" << endl;
             return 0;
