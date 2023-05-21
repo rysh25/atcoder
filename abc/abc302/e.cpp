3e95fe351b90571caecb5a06dcf1e1d6d1074e4b
Ryohei Shiraki <rysh25@gmail.com>
2023-05-21 14:37:28 +0900

    Add 302-E

--- abc/abc302/e.cpp
+++ abc/abc302/e.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 #include <algorithm>
 
 using namespace std;
@@ -9,7 +10,9 @@ int main()
     int N, Q;
     cin >> N >> Q;
 
-    vector<vector<int>> G(N);
+    vector<unordered_set<int>> G(N);
+
+    int ans = N;
 
     for (int i = 0; i < Q; i++)
     {
@@ -20,15 +23,39 @@ int main()
         {
             int u, v;
             cin >> u >> v;
-            G[u].push_back(v);
-            G[v].push_back(u);
+
+            u--, v--;
+
+            // cout << "u=" << u << ", v=" << v << endl;
+
+            if (G[u].size() == 0)
+                ans--;
+            if (G[v].size() == 0)
+                ans--;
+
+            G[u].insert(v);
+            G[v].insert(u);
         }
         else
         {
             int v;
             cin >> v;
-            G[v].clear();
+
+            v--;
+
+            if (G[v].size() != 0)
+            {
+                for (int u : G[v])
+                {
+                    G[u].erase(v);
+                    if (G[u].size() == 0)
+                        ans++;
+                }
+                G[v].clear();
+                ans++;
+            }
         }
+        cout << ans << '\n';
     }
 
     return 0;
