ed9816bb5b7859d8bd982faa44640c88ab188f11
Ryohei Shiraki <rysh25@gmail.com>
2023-10-12 10:38:09 +0900

    Fix

--- abc/abc075/b.cpp
+++ abc/abc075/b.cpp
@@ -30,31 +30,32 @@ int main()
     int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
     int dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
 
-    for (int i = 0; i < H; i++)
+    for (int h = 0; h < H; h++)
     {
-        for (int j = 0; j < W; j++)
+        for (int w = 0; w < W; w++)
         {
-            for (int k = 0; k < 8; k++)
+            for (int i = 0; i < 8; i++)
             {
-                int x = i + dx[k];
-                int y = j + dy[k];
-                if (0 <= x && x < H &&
-                    0 <= y && y < W && S[x][y] == '#')
-                {
-                    ans[i][j]++;
-                }
+                int nh = h + dy[i];
+                int nw = w + dx[i];
+
+                if (nh < 0 || H <= nh || nw < 0 || W <= nw)
+                    continue;
+
+                if (S[nh][nw] == '#')
+                    ans[h][w]++;
             }
         }
     }
 
-    for (int i = 0; i < H; i++)
+    for (int h = 0; h < H; h++)
     {
-        for (int j = 0; j < W; j++)
+        for (int w = 0; w < W; w++)
         {
-            if (S[i][j] == '#')
+            if (S[h][w] == '#')
                 cout << '#';
             else
-                cout << ans[i][j];
+                cout << ans[h][w];
         }
 
         cout << endl;
