f80e4aa7f706c76a33fa3a63e383677e94fdc852
Ryohei Shiraki <rysh25@gmail.com>
2023-11-05 22:08:50 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,17 +1,36 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <list>
+#include <utility>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <climits>
+#include <cstring>
+// #include <cstdlib>
+#include <limits>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <tuple>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
 using namespace std;
-// #include <atcoder/all>
-// using namespace atcoder;
+
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rep1(i, n) for (int i = 1; i < (int)(n + 1); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n); i++)
+
+using ll = long long;
 
 int main()
 {
-    int n;
-    cin >> n;
 
-    int ans = 0;
-
-    cout << ans << endl;
     return 0;
 }
